646c1c71f4733bfc36d0439cb04c09a1ff9e335c
Odafe kevin <xsaysoft@gmail.comm>
2020-04-29 12:21:58 +0100

    change sever

--- run.py
+++ run.py
@@ -4,8 +4,8 @@ from logging.handlers import RotatingFileHandler
 import os
 if __name__ == "__main__":
     app = create_app("config")
-    socketio.run(app, debug=True ,host='127.0.0.1',port=5002)
-    #socketio.run(app, debug=True ,host='0.0.0.0',port=5001)
+    #socketio.run(app, debug=True ,host='127.0.0.1',port=5002)
+    socketio.run(app, debug=True ,host='0.0.0.0',port=5001)
     #uncomment when pushing to docker hub
     #app.run(debug=True,host='0.0.0.0')
     #push to server ,host='192.168.88.37'
